62c5b81fcd4ae152e97c148eb931fee6cb9472fa
Rochdy-max <rochdy.bachabi@epitech.eu>
2023-12-07 17:18:15 +0100

    feat: Adding 'NeuronsLayer' acting as an encapsulation of a layer in a neural network

---
+++ mytorch/neurons_layer.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+class NeuronsLayer:
+    def __init__(self, length: int, prev_length: int) -> None:
+        """
+            Initialize the weigths matrix and biases vector for this layer.
+
+            Each line from the weights matrix contains the weigths of the
+            synapses binding the 'prev_length' neurons of the preceding layer
+            to a single neuron of the current layer.
+
+            For the 'length' lines of this matrix (one binding each neuron to
+            those of the previous layer) there are 'prev_length' columns. This
+            is a "length x prev_length" matrix intialized to random values at this time.
+            
+            The biases vector also has 'length' lines and each express a value
+            to add to the weigthed sum of the previous layer's neurons' activations
+            considering the treshold from which this sum should be to activate
+            the neuron. At the time, this vector contains only zeros.
+        """
+        self.weigths = np.random.randn(length, prev_length)
+        self.biases = np.zeros(length)
+
+    def activation(self, z: np.ndarray) -> np.ndarray:
+        """
+            ReLU function applied to the weighted sum matrix ('z') to determine
+            activations for the layer's neurons and return a matrix containing
+            these activations.
+            
+            There can be many activations for each neuron if there are multiple
+            examples. One column correspond to one example.
+        """
+        return np.max(np.zeros(z.shape), z)
+    
+    def process(self, X) -> np.ndarray:
+        """
+            Process the input matrix 'X' to determine the activations for the
+            layer's neurons and return a matrix containing these activations.
+
+            This operation consist of calculating the weighted sum matrix 'z'
+            before passing it to the activation function which will compute
+            the neurons' activation as suggested.
+            
+            'z' and the resulting matrix 'y_hat' will have the same number of
+            columns as 'X', each corresponding to an example. The number
+            of lines will be the same as the number of layer's neurons (which
+            is also the number of lines in the weights matrix).
+            
+            z = w.X + b
+            y_hat = activation(z)
+        """
+        z = np.dot(self.weigths, X) + self.biases
+        return self.activation(z)
