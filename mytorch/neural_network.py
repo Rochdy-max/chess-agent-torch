b29ebdd3c819db28a0fcdb060b3e5f1ee190315e
Rochdy-max <rochdy.bachabi@epitech.eu>
2023-12-14 14:16:16 +0100

    feat: add implementation for NeuronsNetwork class constructor

--- mytorch/neural_network.py
+++ mytorch/neural_network.py
@@ -1,17 +1,27 @@
 import numpy as np
+from neurons_layer import NeuronsLayer
 
 class NeuralNetwork:
-    def __init__(self, layers_count: int, *sizes: int) -> None:
-        pass
+    """
+        Provides an encapsulation for a neural network
+    """
+
+    def __init__(self, *sizes: int) -> None:
+        """
+            Initializes the characteristic data for a neural network :
+            number of layers, weights matrices based on the parameter 'sizes'.
+        """
+        self.layers_count = len(sizes)
+        self.layers = list()
+        for i in range(1, self.layers_count):
+            self.layers.append(NeuronsLayer(sizes[i], sizes[i - 1]))
+            
     
     def forward_prop(self, X: np.ndarray) -> np.ndarray:
         pass
-    
-    def activation(self, z: np.ndarray) -> np.ndarray:
+
+    def compute_cost(y: np.ndarray, y_hat: np.ndarray) -> float:
         pass
-    
-    def compute_cost(self, y: np.ndarray, y_hat: np.ndarray) -> float:
-        pass
-    
+            
     def back_prop(self, y: np.ndarray) -> None:
         pass
