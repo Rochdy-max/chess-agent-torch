dfbab318887a960b0a57379b88205812d5f9d987
Rochdy-max <rochdy.bachabi@epitech.eu>
2023-12-26 14:07:35 +0100

    refactor: interact with factory classes for activations and cost functions and derivatives

--- mytorch/neural_network.py
+++ mytorch/neural_network.py
@@ -1,17 +1,19 @@
 import numpy as np
 from intersynaptic_space import IntersynapticSpace
-from activation_derivatives import ActivationDerivatives
-from activation_functions import ActivationFunctions
+from activation_derivatives import ActivationDerivativesFactory
+from activation_functions import ActivationFunctionsFactory
+from cost_derivatives import CostDerivativesFactory
+from cost_functions import CostFunctionsFactory
 
 class NeuralNetwork:
     """
-        Provides an encapsulation for a neural network
+    Provides an encapsulation for a neural network.
     """
 
     def __init__(self, *sizes: int) -> None:
         """
-            Initializes the characteristic data for a neural network :
-            number of layers, weights matrices based on the parameter 'sizes'.
+        Initializes the characteristic data for a neural network :
+        number of layers, weights matrices based on the parameter 'sizes'.
         """
         self.interspaces_count = len(sizes)
         self.interspaces = list()
@@ -24,15 +26,16 @@ class NeuralNetwork:
 
     def forward_prop(self, X: np.ndarray) -> np.ndarray:
         """
-            Applies forward propagation on an input matrix 'X' with lines
-            for the activation values of this network input layer's neurons.
-            
-            Each line of this matrix should correspond to an example.
-            
-            The returned matrix 'y_hat' has the same architecture.
-            Each column is bound to a neuron of the last layer and the
-            columns are bound to examples.
+        Apply forward propagation on an input matrix 'X' with lines
+        for the activation values of this network input layer's neurons.
+
+        Each line of this matrix should correspond to an example.
+
+        The returned matrix 'y_hat' has the same architecture.
+        Each column is bound to a neuron of the last layer and the
+        columns are bound to examples.
         """
+        activation_f = ActivationFunctionsFactory.deliver("sigmoid")
         A = np.atleast_2d(X).T # Using the transposition of X for computation
 
         if A.shape[0] != self.interspaces[0].weights.shape[1]:
@@ -43,7 +46,7 @@ class NeuralNetwork:
         for interspace in self.interspaces:
             z = interspace.process(A)
             self.cache["z"].append(z)
-            A = ActivationFunctions.sigmoid(z)
+            A = activation_f(z)
             self.cache["A"].append(A)
 
         # The last layer activation matrix (A) is equal to the transposition of y_hat
@@ -51,36 +54,30 @@ class NeuralNetwork:
     
     def compute_cost(y: np.ndarray, y_hat: np.ndarray) -> float:
         """
-            Cost function determining the accuracy of a neural network.
-            It uses the Cross Entropy Loss function, typically serving
-            multi-class and multi-label classifications.
-
-            J = -SUM(T * S)
-            
-            T := The expected target from y
-            S := The corresponding prediction (or approximation) from y_hat
+        Cost function determining the accuracy of a neural network.
         """
-        sum_val = 0
-
+        cost_f = CostFunctionsFactory.deliver("cross_entropy")
         y = np.atleast_2d(y)
         y_hat = np.atleast_2d(y_hat)
+
         if y.shape != y_hat.shape:
             raise ValueError(f"y and y_hat should have the same shape ({y.shape} != {y_hat.shape})")
 
-        # For each example, add result of the computation S*T to the total_sum
-        for t, s in zip(y, y_hat):
-            sum_val += np.dot(t, s)
-
-        return -sum_val
+        return cost_f(y, y_hat)
 
     def back_prop(self, y: np.ndarray) -> dict[str, list]:
-        dA = -np.atleast_2d(y).T # derivative of the cost function
+        """
+        Return a dictionary containing the gradients to apply to the weights and biases of the network
+        """
+        activation_prime = ActivationDerivativesFactory.deliver("sigmoid")
+        cost_prime = CostDerivativesFactory.deliver("cross_entropy")
+        dA = cost_prime(np.atleast_2d(y).T, self.cache["A"][-1]) # derivative of the cost function
         grads_W = list()
         grads_b = list()
         i = -1 # start from the end of the cache
 
         for interspace in reversed(self.interspaces):
-            dz = ActivationDerivatives.sigmoid_prime(self.cache["z"][i]) * dA # derivative of the activation funtion (ReLU) * dA
+            dz = activation_prime(self.cache["z"][i]) * dA # derivative of the activation funtion * dA
             dW = np.dot(dz, self.cache["A"][i - 1].T) # dW = dz . T(A_prev)
             grads_W.append(dW)
             db = dz # db = dz * 1
@@ -94,10 +91,13 @@ class NeuralNetwork:
         ])
 
     def apply_gradient_descent(self, learning_rate: float, dW: list[np.ndarray], db: list[np.ndarray]) -> None:
+        """
+        Apply the gradient descent to improve the neural network
+        """
         for i in range(len(self.interspaces)):
             # Last In First Out for the gradients
-            self.interspaces[i].weights = self.interspaces[i].weights - (learning_rate * dW.pop())
-            self.interspaces[i].biases = self.interspaces[i].biases - (learning_rate * db.pop())
+            self.interspaces[i].weights -= learning_rate * dW.pop()
+            self.interspaces[i].biases -= learning_rate * db.pop()
 
 def test_compute_cost():
     z = np.zeros((4, 5))
@@ -130,7 +130,7 @@ def test_compute_cost():
 def test_neural_network():
     nn = NeuralNetwork(3, 4, 2)
     X = np.array([4, 3, 1])
-    y = np.array([1, 2])
+    y = np.array([0.5, 0.8])
     epochs = 10
 
     for _ in range(epochs):
@@ -139,8 +139,7 @@ def test_neural_network():
         cost = NeuralNetwork.compute_cost(y, y_hat)
         print(f"{cost=}")
         grads = nn.back_prop(y)
-        nn.apply_gradient_descent(1, grads['weights'], grads['biases'])
-    
+        nn.apply_gradient_descent(0.1, grads['weights'], grads['biases'])
 
 if __name__ == "__main__":
     # test_compute_cost()
