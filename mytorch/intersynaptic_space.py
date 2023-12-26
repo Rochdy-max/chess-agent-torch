926eb9db2f3e1b12ea921be8e4b03c610ed2d0f8
Rochdy-max <rochdy.bachabi@epitech.eu>
2023-12-26 13:56:50 +0100

    style: remove indentation before docstrings contents

--- mytorch/intersynaptic_space.py
+++ mytorch/intersynaptic_space.py
@@ -3,25 +3,25 @@ from utils import array_map
 
 class IntersynapticSpace:
     """
-        Provides an encapsulation for the data involved between two layers in a neural network
+    Provides an encapsulation for the data involved between two layers in a neural network
     """
 
     def __init__(self, size: int, prev_size: int) -> None:
         """
-            Initialize the size, weigths matrix and biases vector for this layer.
+        Initialize the size, weigths matrix and biases vector for this layer.
 
-            Each line from the weights matrix contains the weigths of the
-            synapses binding the 'prev_size' neurons of the preceding layer
-            to a single neuron of the current layer.
+        Each line from the weights matrix contains the weigths of the
+        synapses binding the 'prev_size' neurons of the preceding layer
+        to a single neuron of the current layer.
 
-            For the 'size' lines of this matrix (one binding each neuron to
-            those of the previous layer) there are 'prev_size' columns. This
-            is a "size x prev_size" matrix intialized to random values at this time.
+        For the 'size' lines of this matrix (one binding each neuron to
+        those of the previous layer) there are 'prev_size' columns. This
+        is a "size x prev_size" matrix intialized to random values at this time.
             
-            The biases vector also has 'size' lines and each express a value
-            to add to the weigthed sum of the previous layer's neurons' activations
-            considering the treshold from which this sum should be to activate
-            the neuron. At the time, this vector contains only zeros.
+        The biases vector also has 'size' lines and each express a value
+        to add to the weigthed sum of the previous layer's neurons' activations
+        considering the treshold from which this sum should be to activate
+        the neuron. At the time, this vector contains only zeros.
         """
         self.size = size
         self.weights = np.random.randn(size, prev_size)
@@ -29,20 +29,20 @@ class IntersynapticSpace:
     
     def process(self, X: np.ndarray) -> np.ndarray:
         """
-            Process the input matrix 'X' to determine the activations for the
-            layer's neurons and return a matrix containing these activations.
+        Process the input matrix 'X' to determine the activations for the
+        layer's neurons and return a matrix containing these activations.
 
-            This operation consist of calculating the weighted sum matrix 'z'
-            before passing it to the activation function which will compute
-            the neurons' activation as suggested.
+        This operation consist of calculating the weighted sum matrix 'z'
+        before passing it to the activation function which will compute
+        the neurons' activation as suggested.
             
-            'z' and the returned matrix 'A' will have the same number of
-            columns as 'X', each corresponding to an example. The number
-            of lines will be the same as the number of layer's neurons (which
-            is also the number of lines in the weights matrix).
+        'z' and the returned matrix 'A' will have the same number of
+        columns as 'X', each corresponding to an example. The number
+        of lines will be the same as the number of layer's neurons (which
+        is also the number of lines in the weights matrix).
             
-            z := w * X + b
-            A := activation(z)
+        z := w * X + b
+        A := activation(z)
         """
         if self.weights.shape[1] != X.shape[0]:
             raise ValueError(f"The number of features ({X.shape[0]}) doesn't match the valid input size ({self.weights.shape[1]})")
